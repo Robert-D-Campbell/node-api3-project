c20ebdadecd73a7b729885713f9428d7194b9b2a
Robert Campbell <robertc1191@Roberts-MacBook-Pro-2.local>
2019-12-05 12:42:32 -0800

    updated dynamic port, added .env file

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
+require("dotenv").config();
+
 const server = require("./server.js");
 
-server.listen(4001, () => {
-  console.log("\n* Server Running on http://localhost:4001 *\n");
+const port = process.env.PORT || "4000";
+
+server.listen(port, () => {
+  console.log(`\n* Server Running on http://localhost:${port} *\n`);
 });
